a4d8074668baa8a482db099f95f3003c9969e123
sam-dp <cloudysaga@gmail.com>
2023-08-07 23:50:03 -0700

    Working on JOIN queries

--- src/artentry/queries.js
+++ src/artentry/queries.js
@@ -1,5 +1,6 @@
-const getArtentries = "SELECT * FROM artentry";
-const getArtentryById = "SELECT * FROM artentry WHERE id = $1";
+const getArtentries = "SELECT * FROM artentry JOIN image ON image.artentry_id = artentry.id";
+
+const getArtentryById = "SELECT * FROM artentry WHERE id = $1 ";
 
 module.exports = {
     getArtentries,
